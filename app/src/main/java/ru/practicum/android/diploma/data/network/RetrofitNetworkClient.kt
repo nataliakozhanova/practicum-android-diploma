49b44d447c1ff16d00b9e3d18254681b685346a5
Alex Panic <gm4sp@mail.ru>
2024-07-05 16:00:35 +0500

    detekt fix

--- app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -6,6 +6,7 @@ import android.net.NetworkCapabilities
 import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import ru.practicum.android.diploma.data.NetworkClient
 import ru.practicum.android.diploma.data.dto.Response
 import java.net.HttpURLConnection
@@ -21,9 +22,10 @@ class RetrofitNetworkClient(
         ) as ConnectivityManager
         val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
 
-        return capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || capabilities.hasTransport(
-            NetworkCapabilities.TRANSPORT_WIFI
-        ) || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
+        return capabilities != null &&
+            (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET))
     }
 
     override suspend fun doRequest(dto: Any): Response {
@@ -34,7 +36,7 @@ class RetrofitNetworkClient(
             try {
                 // заглушка на 200, потом будет проверка when(dto)
                 Response().apply { resultCode = HttpURLConnection.HTTP_OK }
-            } catch (e: Throwable) {
+            } catch (e: HttpException) {
                 e.message?.let { Log.e("Http", it) }
                 Response().apply { resultCode = HttpURLConnection.HTTP_INTERNAL_ERROR }
             }
