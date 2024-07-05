532298a452c6503460dadf065dfc1b83224e5bb6
Alex Panic <gm4sp@mail.ru>
2024-07-05 14:35:05 +0500

    добавил костяк Retrofit

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/RetrofitNetworkClient.kt
@@ -0,0 +1,45 @@
+package ru.practicum.android.diploma.data.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.dto.Response
+
+class RetrofitNetworkClient(
+    private val apiService: HhApiService,
+    private val context: Context,
+) : NetworkClient {
+
+    private fun isConnected(): Boolean {
+        val connectivityManager = context.getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            }
+        }
+        return false
+    }
+
+    override suspend fun doRequest(dto: Any): Response {
+        if (!isConnected()) {
+            return Response().apply { resultCode = -1 }
+        }
+        return withContext(Dispatchers.IO) {
+            try {
+                // заглушка на 200, потом будет проверка when(dto)
+                Response().apply { resultCode = 200 }
+            } catch (e: Throwable) {
+                Response().apply { resultCode = 500 }
+            }
+        }
+    }
+}
