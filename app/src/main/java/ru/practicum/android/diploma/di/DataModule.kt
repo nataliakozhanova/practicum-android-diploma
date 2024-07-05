56db8ab12a60c287722ec8888505ebd54e4396cc
Alex Panic <gm4sp@mail.ru>
2024-07-05 18:59:08 +0500

    Начальная настройка DI через koin

---
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -0,0 +1,37 @@
+package ru.practicum.android.diploma.di
+
+import android.content.Context
+import com.google.gson.Gson
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.data.NetworkClient
+import ru.practicum.android.diploma.data.network.HhApiService
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+
+val dataModule = module {
+    single<HhApiService> {
+        Retrofit.Builder()
+            .baseUrl("https://api.hh.ru")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(HhApiService::class.java)
+    }
+
+    single {
+        androidContext().getSharedPreferences(
+            androidContext().getString(R.string.shared_pref_storage_key),
+            Context.MODE_PRIVATE
+        )
+    }
+
+    factory { Gson() }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(get(), androidContext())
+    }
+
+    // TODO: Room
+}
