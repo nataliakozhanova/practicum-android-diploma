51c6d58b8b88ae5220560030c510f7effe747780
Alex Panic <gm4sp@mail.ru>
2024-07-05 15:00:53 +0500

    try to fix detekt

--- app/src/main/java/ru/practicum/android/diploma/data/network/HhApiService.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HhApiService.kt
@@ -1,4 +1,13 @@
 package ru.practicum.android.diploma.data.network
 
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import ru.practicum.android.diploma.data.dto.Response
+
 interface HhApiService {
+    @Headers("Authorization: Bearer YOUR_TOKEN",
+        "HH-User-Agent: Application Name (name@example.com)")
+    @GET("/vacancies/{vacancy_id}")
+    suspend fun getVacancy(@Path("vacancy_id") id: String): Response
 }
