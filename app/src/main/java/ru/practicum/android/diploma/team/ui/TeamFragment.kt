d48ca9018eb886a9b259be30651ab5643450407d
ZarubaGit <lifesteel6@gmail.com>
2024-07-06 12:06:21 +0400

    Добавил экран "Команда" Добавил подходящие шрифты и размеры

--- app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
@@ -1,15 +1,30 @@
 package ru.practicum.android.diploma.team.ui
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_team, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() {
+
+        }
+
+
     }
 }
