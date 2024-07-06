765d543481ce2b7578aed676f21c32c407d67d25
ZarubaGit <lifesteel6@gmail.com>
2024-07-06 12:17:53 +0400

    fix bugs2 for detekt

--- app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
@@ -8,9 +8,7 @@ import android.view.ViewGroup
 import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_team, container, false)
     }
