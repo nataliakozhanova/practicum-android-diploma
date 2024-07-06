27647f1d27b9c1bf4dd9b049d77ee350dc581b1a
ZarubaGit <lifesteel6@gmail.com>
2024-07-06 12:27:57 +0400

    fix bugs3 for detekt добавил binding

--- app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/team/ui/TeamFragment.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentTeamBinding
 
 class TeamFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_team, container, false)
+
+    private var _binding: FragmentTeamBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentTeamBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
