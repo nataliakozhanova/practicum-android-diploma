2ab40d28a6b9a2c3ca360e3f94cd9cc12c31e7da
stoutgeneral <erin1997@yandex.ru>
2024-07-06 21:35:58 +0700

    detekt исправления

--- app/src/main/java/ru/practicum/android/diploma/vacancydetail/presentation/view/VacansyDetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancydetail/presentation/view/VacansyDetailsFragment.kt
@@ -6,5 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class VacansyDetailsFragment: Fragment() {
+class VacansyDetailsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 }
