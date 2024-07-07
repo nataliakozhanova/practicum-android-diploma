0ebed2dc1590a8d08c6ad9b0ab2ece8f65b41ebb
AlexPanic <alexey.panichev@gmail.com>
2024-07-07 13:20:24 +0500

    viewBinding во фрагментах

--- app/src/main/java/ru/practicum/android/diploma/favorites/presentation/view/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/presentation/view/FavoriteFragment.kt
@@ -6,10 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
+import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class FavoriteFragment : Fragment() {
 
+    private var _binding: FragmentFavoriteBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
