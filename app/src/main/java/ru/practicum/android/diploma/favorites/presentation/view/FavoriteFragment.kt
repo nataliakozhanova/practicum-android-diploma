c89cf465d2d8c887c7281443e6f039566b542569
AlexPanic <alexey.panichev@gmail.com>
2024-07-07 14:15:05 +0500

    detekt fix

--- app/src/main/java/ru/practicum/android/diploma/favorites/presentation/view/FavoriteFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/presentation/view/FavoriteFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavoriteBinding
-import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 
 class FavoriteFragment : Fragment() {
 
--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -9,18 +9,19 @@ import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityRootBinding
+    private var _binding: ActivityRootBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityRootBinding.inflate(layoutInflater)
+        _binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentCV) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding!!.menuBNV.setupWithNavController(navController)
+        binding.menuBNV.setupWithNavController(navController)
     }
 
 }
