51b4817ed57595b1c47623308be2772a3351c77a
ZarubaGit <lifesteel6@gmail.com>
2024-07-09 21:44:44 +0400

    fix FavoriteViewHolder, swap on Formatter

--- app/src/main/java/ru/practicum/android/diploma/favorites/presentation/view/FavoriteViewHolder.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/presentation/view/FavoriteViewHolder.kt
@@ -5,8 +5,9 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.common.domain.VacancyBase
 import ru.practicum.android.diploma.databinding.ItemFavoriteViewBinding
-import ru.practicum.android.diploma.favorites.domain.models.FavouriteVacanciesModel
+import ru.practicum.android.diploma.util.Formatter
 
 class FavoriteViewHolder(
     private val binding: ItemFavoriteViewBinding,
@@ -15,24 +16,18 @@ class FavoriteViewHolder(
     private val trackCornerRadius: Int =
         itemView.context.resources.getDimensionPixelSize(R.dimen.logo_corner_radius)
 
-    fun bind(item: FavouriteVacanciesModel) {
-        binding.titleFavoriteItem.text = item.name
-        binding.subtitleFavoriteItem.text = item.employerName
-        binding.priceFavoriteItem.text = formatSalary(item.salaryFrom, item.salaryTo, item.salaryCurrency)
+    private val stringBuilderNameAndCity = StringBuilder()
+
+    fun bind(item: VacancyBase) {
+        binding.titleFavoriteItem.text = stringBuilderNameAndCity
+            .append(item.name + ", " + item.employerInfo.areaName)
+        binding.subtitleFavoriteItem.text = item.employerInfo.employerName
+        binding.priceFavoriteItem.text = Formatter.formatSalary(itemView.context, item.salaryInfo)
 
         Glide.with(itemView)
-            .load(item.employerLogoUrl)
+            .load(item.employerInfo.employerLogoUrl)
             .placeholder(R.drawable.logo_placeholder_image)
             .transform(CenterCrop(), RoundedCorners(trackCornerRadius))
             .into(binding.imageItemFavorite)
-    }
-
-    private fun formatSalary(min: Int?, max: Int?, currency: String): String {
-        return when {
-            min != null && max != null -> "от $min до $max $currency"
-            min != null -> "от $min $currency"
-            max != null -> "до $max $currency"
-            else -> "зарплата не указана"
-        }
     }
 }
