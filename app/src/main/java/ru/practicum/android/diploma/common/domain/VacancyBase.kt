fc53ea4f651cf7ba1160edd099136805d6d01d56
nataliakozhanova <Natalia.v.kozhanova@gmail.com>
2024-07-11 23:38:15 +0200

    детект 3

--- app/src/main/java/ru/practicum/android/diploma/common/domain/VacancyBase.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/domain/VacancyBase.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.common.domain
 open class VacancyBase(
     val hhID: String,
     val name: String,
-    //val area: String,
     val isFavorite: Boolean,
     val employerInfo: EmployerInfo,
     val salaryInfo: SalaryInfo?,
