81c56b2f153ef1e4c3ac6e290c9e5c05bcbbb05b
Nikita <139341914+ZarubaGit@users.noreply.github.com>
2024-07-14 11:18:39 +0400

    bug fix detect

--- app/src/main/java/ru/practicum/android/diploma/vacancydetails/presentation/view/VacancyDetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancydetails/presentation/view/VacancyDetailsFragment.kt
@@ -7,11 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentVacancyDetailsBinding
 import ru.practicum.android.diploma.databinding.ItemVacancyDetailsViewBinding
 import ru.practicum.android.diploma.util.Formatter
