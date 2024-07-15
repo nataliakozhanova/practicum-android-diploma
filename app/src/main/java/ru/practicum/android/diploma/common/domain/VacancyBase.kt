456a22fedff5e3c9d94c6c0dbf8e95c5ce5307d6
AlexPanic <alexey.panichev@gmail.com>
2024-07-15 09:39:52 +0500

    error fix

--- app/src/main/java/ru/practicum/android/diploma/common/domain/VacancyBase.kt
+++ app/src/main/java/ru/practicum/android/diploma/common/domain/VacancyBase.kt
@@ -3,7 +3,7 @@ package ru.practicum.android.diploma.common.domain
 open class VacancyBase(
     val hhID: String,
     val name: String,
-    val isFavorite: Boolean,
+    var isFavorite: Boolean,
     val employerInfo: EmployerInfo,
     val salaryInfo: SalaryInfo?,
 )
