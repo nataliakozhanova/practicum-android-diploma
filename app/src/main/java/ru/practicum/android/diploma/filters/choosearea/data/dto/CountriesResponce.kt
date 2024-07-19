7d313df76c1e7282dc5987802a8016e38ca75a68
AlexPanic <alexey.panichev@gmail.com>
2024-07-19 19:04:27 +0500

    detekt fix

--- app/src/main/java/ru/practicum/android/diploma/filters/choosearea/data/dto/CountriesResponce.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/choosearea/data/dto/CountriesResponce.kt
@@ -3,7 +3,6 @@ package ru.practicum.android.diploma.filters.choosearea.data.dto
 import ru.practicum.android.diploma.common.data.ResponseBase
 import ru.practicum.android.diploma.filters.choosearea.domain.models.CountryInfo
 
-
 class CountriesResponce(val countries: List<CountryInfo>?) : ResponseBase()
 
 data class CountryDto(
