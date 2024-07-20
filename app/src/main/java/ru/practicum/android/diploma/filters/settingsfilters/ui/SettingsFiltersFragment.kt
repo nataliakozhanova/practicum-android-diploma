8f4ae1b879fe3e9141ef1d181a952ed896e90da4
Nikita <139341914+ZarubaGit@users.noreply.github.com>
2024-07-20 19:01:37 +0400

    bug fix detekt

--- app/src/main/java/ru/practicum/android/diploma/filters/settingsfilters/ui/SettingsFiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/settingsfilters/ui/SettingsFiltersFragment.kt
@@ -79,8 +79,12 @@ class SettingsFiltersFragment : Fragment() {
                 updateButtonsVisibility()
                 changeTextInputLayoutEndIconMode(s)
             }
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // so what, detekt?
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // so what, detekt?
+            }
         })
 
         binding.industryLayout.setEndIconOnClickListener {
