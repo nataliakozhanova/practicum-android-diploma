89c2e33a380b0bee37c7d86736329c97a75df3cc
Nikita <139341914+ZarubaGit@users.noreply.github.com>
2024-07-21 18:43:50 +0400

    bug fix detekt

--- app/src/main/java/ru/practicum/android/diploma/filters/choosearea/ui/ChooseRegionFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/choosearea/ui/ChooseRegionFragment.kt
@@ -115,7 +115,9 @@ class ChooseRegionFragment : Fragment() {
         }
 
         binding.tietSearchMask.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // for fu**ing detekt
+            }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 searchDebounce(s.toString())
@@ -128,7 +130,9 @@ class ChooseRegionFragment : Fragment() {
                 }
             }
 
-            override fun afterTextChanged(s: Editable?) {}
+            override fun afterTextChanged(s: Editable?) {
+                // for fu**ing detekt
+            }
         })
 
         binding.editTextRegion.setEndIconOnClickListener {
