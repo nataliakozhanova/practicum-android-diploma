23c5a9ea3f10c2f47e1a63daee0b2e1261706603
nataliakozhanova <Natalia.v.kozhanova@gmail.com>
2024-07-25 13:58:51 +0200

    удален код для авторизации в HhApiService для справочников

--- app/src/main/java/ru/practicum/android/diploma/filters/chooseindustry/data/network/HhApiServiceIndustry.kt
+++ app/src/main/java/ru/practicum/android/diploma/filters/chooseindustry/data/network/HhApiServiceIndustry.kt
@@ -2,21 +2,10 @@ package ru.practicum.android.diploma.filters.chooseindustry.data.network
 
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
-import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.filters.chooseindustry.data.dto.IndustryDto
 
 interface HhApiServiceIndustry {
-    @Headers(
-        USER,
-        AUTH
-    )
     @GET("/industries")
     suspend fun getIndustries(@Query("search") query: String? = null): Response<List<IndustryDto>>
-
-    companion object {
-        const val USER = "HH-User-Agent: PracticumHHCareerCompass (natalia.v.kozhanova@gmail.com)"
-        const val AUTH = "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
-    }
 }
